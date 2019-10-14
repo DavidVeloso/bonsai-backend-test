748395de569149587b9f62ec4084274bc799456b
David Veloso <davidedsoon@gmail.com>
2019-10-14 19:05:59 -0300

    Add ticket resolver test

--- source/main.ts
+++ source/main.ts
@@ -1,15 +1,11 @@
 import { ApolloServer } from "apollo-server"
-import { ObjectId } from "mongodb"
 import * as dotenv from "dotenv"
 import * as path from "path"
 import "reflect-metadata"
-import { buildSchema } from "type-graphql"
 
 import { connectDb } from "./config/database"
-import { ObjectIdScalar } from "./objectId.scalar"
-import resolvers from "./modules"
-import typegooseMiddleware from "./middlewares/typegoose.middleware"
 import { logger } from "./config/logger"
+import { graphqlSchema } from './graphqlSchema'
 
 const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env'
 const envPath = path.resolve(`${__dirname}/../${envFile}`)
@@ -21,12 +17,7 @@ const {SERVER_PORT, DATABASE_URI} = process.env
 const main = async () => {
   try {
     await connectDb(<string>DATABASE_URI)
-    const schema = await buildSchema({
-      resolvers,
-      emitSchemaFile: path.resolve(__dirname, "schema.gql"),
-      globalMiddlewares: [typegooseMiddleware],
-      scalarsMap: [{ type: ObjectId, scalar: ObjectIdScalar }],
-    })
+    const schema = await graphqlSchema()
     const server = new ApolloServer({ schema, context: {} })
     const { url } = await server.listen(SERVER_PORT)
     logger.info(`GraphQL Playground running at ${url}`)
